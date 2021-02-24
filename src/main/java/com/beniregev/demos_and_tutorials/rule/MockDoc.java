8be239289ee31df41564c9ef557a119d97291724
beni.regev@finastra.com <qe7uwt2v!>
2021-02-24 09:15:33 +0200

    Updating MockDoc class JavaDoc comment

--- src/main/java/com/beniregev/demos_and_tutorials/rule/MockDoc.java
+++ src/main/java/com/beniregev/demos_and_tutorials/rule/MockDoc.java
@@ -6,11 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * @author Binyamin Regev
  * An annotation that can be placed on the mocking methods referenced
  * by the annotation FixtureConfig on the unit test. Can be used to provide
  * insight into what the mocking method is doing for reporting purposes.
  * Leveraged by {@see DocRule}
+ * @author Binyamin Regev
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
