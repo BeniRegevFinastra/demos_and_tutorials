b4e59750c02f291822e6d6c1f3b9a8c9dba26443
beni.regev@finastra.com <qe7uwt2v!>
2021-02-24 09:25:22 +0200

    Updating JavaDoc comment for method Map<Integer,MailGenerator> generatorMap(List<MailGenerator>) in MainConfiguration claas.

--- src/main/java/com/beniregev/demos_and_tutorials/never_use_switch_example/MainConfiguration.java
+++ src/main/java/com/beniregev/demos_and_tutorials/never_use_switch_example/MainConfiguration.java
@@ -24,8 +24,33 @@ import static java.util.stream.Collectors.toMap;
 @ComponentScan
 @EnableScheduling
 public class MainConfiguration {
+
+    /**
+     * <div>
+     *     <p>
+     *         Because all classes that implement {@link MailGenerator} are
+     *         annotated with Spring framework <i>@Component</i> annotation
+     *         and this class is annotated with Spring framework
+     *         <i>ComponentScan</i> annotation then Spring framework already
+     *         scanned all the packages of the application and gathered them.
+     *     </p>
+     *     <p>
+     *         This method receives the collection of those classes and
+     *         collects them into a {@link Map} to be used later. And how this
+     *         method is called? Well, in <i>IntelliJ IDEA</i> press
+     *         <i><b>CTRL</b></i> key and click on the method name, and you
+     *         will see.
+     *     </p>
+     *     <p>
+     *         <b>Hint:</b> The method is annotated with Spring framework
+     *         <i>@Bean</i> annotation, what that means?
+     *     </p>
+     * </div>
+     * @param mailGenerators
+     * @return
+     */
     @Bean
-    public Map<Integer,MailGenerator> generatorMap(List<MailGenerator> mailGenerators){
+    public Map<Integer,MailGenerator> generatorMap(List<MailGenerator> mailGenerators) {
         return mailGenerators.stream().collect(toMap(MailGenerator::getMyCode, identity()));
     }
 
